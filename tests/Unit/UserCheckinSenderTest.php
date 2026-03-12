5eda1f0eb41ce4b328b6d9921bdf44b9fcb02440
jarno-js <jarno.janssens@student.ehb.be>
2026-03-12 11:03:43 +0100

    test: add UserCheckinSender unit tests

---
+++ tests/Unit/UserCheckinSenderTest.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class UserCheckinSenderTest extends TestCase
+{
+    private UserCheckinSender $sender;
+
+    protected function setUp(): void
+    {
+        $mockClient = $this->createMock(RabbitMQClient::class);
+        $this->sender = new UserCheckinSender($mockClient);
+    }
+
+    public function test_throws_exception_when_user_id_is_missing(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->sender->send([
+            'badge_id' => 'nfc-badge-abc123',
+        ]);
+    }
+
+    public function test_throws_exception_when_badge_id_is_missing(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->sender->send([
+            'user_id' => 'uuid-v4-hier',
+        ]);
+    }
+
+    public function test_valid_data_builds_correct_xml(): void
+    {
+        $xml = $this->sender->buildXml([
+            'user_id' => 'uuid-v4-hier',
+            'badge_id' => 'nfc-badge-abc123',
+        ]);
+
+        $this->assertStringContainsString('<event_type>user.checkin</event_type>', $xml);
+        $this->assertStringContainsString('<user_id>uuid-v4-hier</user_id>', $xml);
+        $this->assertStringContainsString('<badge_id>nfc-badge-abc123</badge_id>', $xml);
+    }
+}
