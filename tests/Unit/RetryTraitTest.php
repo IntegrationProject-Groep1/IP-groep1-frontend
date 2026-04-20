c2dfea8fa7c1dc875476cb8a7c4c79b5a7a343d9
Sejeongi <125409975+Hiichan75@users.noreply.github.com>
2026-04-20 13:24:34 +0200

    Refactor test_throws_exception_after_max_retries method

--- tests/Unit/RetryTraitTest.php
+++ tests/Unit/RetryTraitTest.php
@@ -29,20 +29,25 @@ class RetryTraitTest extends TestCase
     }
 
     public function test_throws_exception_after_max_retries(): void
-    {
-        $mock = new class {
-            use RetryTrait;
+{
+    $mock = new class {
+        use RetryTrait;
 
-            public function run(callable $fn): void
-            {
-                $this->sendWithRetry($fn, 3, 0);
-            }
-        };
+        public function run(callable $fn): void
+        {
+            $this->sendWithRetry($fn, 3, 0);
+        }
+    };
 
-        $this->expectException(\Exception::class);
+    $this->expectException(\Exception::class);
+    $this->expectExceptionMessage('Connection failed');
 
+    ob_start();
+    try {
         $mock->run(function () {
             throw new \Exception('Connection failed');
         });
+    } finally {
+        ob_end_clean();
     }
 }
